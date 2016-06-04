271b0d8090b1db42f70d9800df37ceec18b07349
Adam Pajkert <pajadam@linux.pl>
2016-06-04 14:45:19 +0200

    Test Graphics with Blinking

--- src/boot.asm
+++ src/boot.asm
@@ -2,7 +2,29 @@
 	; Clear interrupts flag
 	cli 
 	
-	; code here
+		; Video Memory
+		mov ax, 0xb800 
+		mov fs, ax
+		
+		; COLOR  CHARACTER ASCII
+		; 0x42   20
+		; SCREEN SIZE 80 * 25 | 2 BYTES
+		
+		mov bx, 0 ; position
+		mov ax, 0xF054 ; data
+		mov [fs:bx], ax ; set
+		
+		mov bx, 2 ; position
+		mov ax, 0xF045 ; data
+		mov [fs:bx], ax ; set
+		
+		mov bx, 4 ; position
+		mov ax, 0xF053 ; data
+		mov [fs:bx], ax ; set
+		
+		mov bx, 6 ; position
+		mov ax, 0xF054 ; data
+		mov [fs:bx], ax ; set
 	
 	jmp loop
  
